e27b2d27fd39fe31461454e97d5a24d9d532c4e2
faaip <frederikjuutilainen@gmail.com>
2016-04-13 22:28:47 +0200

    Voronoi implemented

---
+++ src/Voronoi.cpp
@@ -0,0 +1,9 @@
+//
+//  Voronoi.cpp
+//  Bachelor
+//
+//  Created by Frederik Tollund Juutilainen on 13/04/16.
+//
+//
+
+#include "Voronoi.hpp"
---
+++ src/Voronoi.hpp
@@ -0,0 +1,14 @@
+//
+//  Voronoi.hpp
+//  Bachelor
+//
+//  Created by Frederik Tollund Juutilainen on 13/04/16.
+//
+//
+
+#ifndef Voronoi_hpp
+#define Voronoi_hpp
+
+#include <stdio.h>
+
+#endif /* Voronoi_hpp */
